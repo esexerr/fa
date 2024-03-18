40fda175fed01e510ec44686e641168e4677f399
goddidt <163083047+esexerr@users.noreply.github.com>
2024-03-17 22:31:52 -0400

    Add files via upload

---
+++ cdn-cgi/apps/head/7_N3aHJ6fMx1u8oH90iZ0LNZJvY.js
@@ -0,0 +1,1 @@
+No Content: https://dose.lol/cdn-cgi/apps/head/7_N3aHJ6fMx1u8oH90iZ0LNZJvY.js
---
+++ static/app.js
@@ -0,0 +1,2 @@
+const here = document.querySelector(".here");
+document.querySelector("#get-link").textContent = window.location.pathname.slice(1);
